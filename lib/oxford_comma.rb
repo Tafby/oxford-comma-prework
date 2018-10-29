a405ba81d598b08cc5307be358824e4a181260d7
Tiffany Carter <tafby88@gmail.com>
2018-10-29 18:36:16 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,6 @@ def oxford_comma(array)
   if array.length == 1 
     return array.join
   elsif array.length == 2 
-    array.join(" and ")
     return array.join(" and ")
   else array.split()
   end
